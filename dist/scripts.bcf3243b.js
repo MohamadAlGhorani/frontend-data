5442b70277f5a8aba7abdb9c012104859b7a91d0
MohamadAlGhorani <moh.gh266@gmail.com>
2019-11-26 15:29:48 +0100

    test

--- dist/scripts.bcf3243b.js
+++ dist/scripts.bcf3243b.js
@@ -8620,7 +8620,7 @@ require("./dataOphalen.js");
 
 require("babel-polyfill");
 
-console.log("hello world!");
+console.log('ik ben blijjjj');
 },{"../styles.scss":"styles.scss","./dataOphalen.js":"scripts/dataOphalen.js","babel-polyfill":"../node_modules/babel-polyfill/lib/index.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -8649,7 +8649,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49543" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50089" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
